cadc25ac988c1e6cef97504892efa76522399087
Ricardo <coelhora@gmail.com>
2021-10-10 19:13:47 +0000

    add INT dissector no receive

--- skeleton/TP-skel/receive.py
+++ skeleton/TP-skel/receive.py
@@ -5,9 +5,10 @@ import os
 
 from scapy.all import sniff, sendp, hexdump, get_if_list, get_if_hwaddr
 from scapy.all import Packet, IPOption
-from scapy.all import ShortField, IntField, LongField, BitField, FieldListField, FieldLenField
+from scapy.all import ShortField, IntField, LongField, BitField, FieldListField, FieldLenField, ByteField
 from scapy.all import IP, TCP, UDP, Raw
 from scapy.layers.inet import _IPOption_HDR
+from scapy.all import bind_layers
 
 def get_if():
     ifs=get_if_list()
@@ -21,6 +22,7 @@ def get_if():
         exit(1)
     return iface
 
+######################Essa classe era demo
 class IPOption_MRI(IPOption):
     name = "MRI"
     option = 31
@@ -33,12 +35,32 @@ class IPOption_MRI(IPOption):
                                    [],
                                    IntField("", 0),
                                    length_from=lambda pkt:pkt.count*4) ]
+
+#####################################################
+
+
+#Nossa classe
+class INT(Packet):
+    name = "INT packet"
+    fields_desc=[ IntField("Tamanho_Filho",0),
+                  IntField("Quantidade_Filhos",0),
+                  ByteField("next_header", 6)]
+    #TODO: Parsear filhos de acordo com o os valores do IntPai
+    
+#####################################################
+
+
+#funcao chamada a cada pacote que chega
 def handle_pkt(pkt):
-    if TCP in pkt and pkt[TCP].dport == 1234:
-        print "got a packet"
+    
+    #if IP in pkt and pkt[IP].proto == 150:
+    if INT in pkt and pkt[INT].next_header!= 1:
+        print "got the packet"
         pkt.show2()
-    #    hexdump(pkt)
-        sys.stdout.flush()
+#    if TCP in pkt and pkt[TCP].dport == 1234:
+#        print "got a packet"
+#    #    hexdump(pkt)
+    sys.stdout.flush()
 
 
 def main():
@@ -46,6 +68,12 @@ def main():
     iface = ifaces[0]
     print "sniffing on %s" % iface
     sys.stdout.flush()
+
+    #Adiciona o INT na stack, apos o parser do IP.
+    bind_layers(IP, INT, proto=150)
+#    Packet.bind_layers(IP, TCP, "proto= 0x06")
+#    Packet.bind_layers(IP, UDP, "proto= 0x11")
+
     sniff(iface = iface,
           prn = lambda x: handle_pkt(x))
 
