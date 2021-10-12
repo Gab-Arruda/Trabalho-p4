b27ccde4ad7a4d6b56f672b47908b707a1bb9ce4
Ricardo <coelhora@gmail.com>
2021-10-12 05:48:17 +0000

    alteracoes para topologia2

--- skeleton/TP-skel/config.sh
+++ skeleton/TP-skel/config.sh
@@ -1,3 +1,4 @@
 simple_switch_CLI --thrift-port 9090 < commands-s1.txt && echo loaded s1 commands
 simple_switch_CLI --thrift-port 9091 < commands-s2.txt && echo loaded s2 commands
 simple_switch_CLI --thrift-port 9092 < commands-s3.txt && echo loaded s3 commands
+simple_switch_CLI --thrift-port 9093 < commands-s4.txt && echo loaded s4 commands
---
+++ skeleton/TP-skel/send.sh
@@ -0,0 +1,4 @@
+./send.py 10.0.1.1 "MENSAGEM de h5 para h1"
+./send.py 10.0.2.2 "MENSAGEM de h5 para h2"
+./send.py 10.0.3.3 "MENSAGEM de h5 para h3"
+./send.py 10.0.4.4 "MENSAGEM de h5 para h4"
